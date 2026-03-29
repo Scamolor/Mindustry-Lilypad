2e24ee6f782eec8a3cbc588bd4501b0809ec5f77
Scamolor <96638432+Scamolor@users.noreply.github.com>
2026-03-28 19:25:51 -0700

    power node/beam link laser lighting disabled until i can figure out why they cause the lighting to have a seizure

--- core/src/mindustry/world/blocks/power/PowerNode.java
+++ core/src/mindustry/world/blocks/power/PowerNode.java
@@ -188,7 +188,7 @@ public class PowerNode extends PowerBlock{
             vx = Mathf.cosDeg(angle1), vy = Mathf.sinDeg(angle1),
             len1 = size1 * tilesize / 2f - 1.5f, len2 = size2 * tilesize / 2f - 1.5f;
 
-        Drawf.laser(laser, laserEnd, laserEnd, x1 + vx*len1, y1 + vy*len1, x2 - vx*len2, y2 - vy*len2, laserScale, light);
+        Drawf.laser(laser, laserEnd, laserEnd, x1 + vx*len1, y1 + vy*len1, x2 - vx*len2, y2 - vy*len2, laserScale, false);
     }
 
     protected boolean overlaps(float srcx, float srcy, Tile other, Block otherBlock, float range){
